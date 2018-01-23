d084db2a3c2b9885b93beedee8964fdb9f7f4b90
Shwetali <shwetali_berad@yahoo.com>
2018-01-23 11:12:49 +0530

    added java file

---
+++ src/EvenNumber.java
@@ -0,0 +1,18 @@
+
+public class EvenNumber {
+	public boolean isEvenNumber(int number){
+        
+        boolean result = false;
+        if(number%2 == 0){
+            result = true;
+        }
+        return result;
+    }
+	
+	public static void main(String[] args) {
+		EvenNumber ev = new EvenNumber();
+		System.out.println("Is Even Number? "+ev.isEvenNumber(2));
+		
+	}
+	
+}
